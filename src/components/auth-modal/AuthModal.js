968c098e5579678dcb0bfa3e251640d506a29b65
Sohail737 <asohail737@gmail.com>
2021-05-06 15:22:38 +0530

    fix:close modal on clicking overlay

--- src/components/auth-modal/AuthModal.js
+++ src/components/auth-modal/AuthModal.js
@@ -1,24 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./AuthModal.module.css";
 
-export const AuthModal = ({ setOpenModal,path }) => {
+export const AuthModal = ({ setOpenModal, path }) => {
   const navigate = useNavigate();
 
   return (
-    <div className={styles.modalContainer}>
+    <div
+      onClick={() => setOpenModal((openModal) => (openModal = false))}
+      className={styles.modalContainer}
+    >
       <div className={styles.authModalContentContainer + " modal"}>
         <h3 className="modal-heading">Want to access premium features?</h3>
         <div className="modal-content">Login to continue</div>
 
         <div className="modal-footer">
           <button
-            onClick={()=>navigate("/account/login",{state:{from:path}})}
+            onClick={() =>
+              navigate("/account/login", { state: { from: path } })
+            }
             className="btn primary outline"
           >
             Login
           </button>
           <button
-             onClick={() => setOpenModal((openModal) => (openModal = false))}
+            onClick={() => setOpenModal((openModal) => (openModal = false))}
             className="btn primary outline margin-left"
           >
             Cancel
